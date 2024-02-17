fb728e20de368e4340af7c023cd9a1b835881e32
DanielaGo25 <dagoa02@gmail.com>
2024-02-16 23:32:48 -0500

    Primera version

---
+++ Actividad_Interpolacion.py
@@ -0,0 +1,7 @@
+from scipy.interpolate import lagrange
+import numpy as np
+import matplotlib.pyplot as plt
+
+deltha_grad = [20,40,60,80,100,120,140,160,180]
+tetha = [170, 160, 150, 140, 130, 120, 110, 100, 90] 
+
