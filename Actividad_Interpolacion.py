163a2ec7ab109092109125c85e856a972bb0fa5a
DanielaGo25 <dagoa02@gmail.com>
2024-02-19 23:09:56 -0500

    Quinta version

--- Actividad_Interpolacion.py
+++ Actividad_Interpolacion.py
@@ -1,4 +1,5 @@
 from scipy.interpolate import lagrange
+from scipy.interpolate import InterpolatedUnivariateSpline
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -18,34 +19,51 @@ ratiosV3 = [2.075, 2.050, 2.025, 1.975, 1.900, 1.825, 1.750, 1.675, 1.575] #L1/L
 
 delthaX = 20 #Segmento para el cual se desea determinar las dimensiones del mecanismo
 
-#Interpolaciones con las proporciones 
-
-Inter_tetha_ratiosV1 = lagrange(tetha, ratiosV1)
-Inter_tetha_ratiosV2 = lagrange(tetha, ratiosV2)
-Inter_tetha_ratiosV3 = lagrange(tetha, ratiosV3)
-
-Inter_tetha_ratiosR1 = lagrange(tetha, ratiosR1)
-Inter_tetha_ratiosR2 = lagrange(tetha, ratiosR2)
-Inter_tetha_ratiosR3 = lagrange(tetha, ratiosR3)
-
+#Interpolaciones empleando lagrange 
+print('\nInterpolaciones empleando lagrange: ')
 Inter_grad_ratiosV1 = lagrange(deltha_grad, ratiosV1)
 Inter_grad_ratiosV2 = lagrange(deltha_grad, ratiosV2)
 Inter_grad_ratiosV3 = lagrange(deltha_grad, ratiosV3)
 
 Inter_grad_ratiosR1 = lagrange(deltha_grad, ratiosR1)
 Inter_grad_ratiosR2 = lagrange(deltha_grad, ratiosR2)
-Inter_grad_ratiosR3 = lagrange(deltha_grad, ratiosR1)
+Inter_grad_ratiosR3 = lagrange(deltha_grad, ratiosR3)
 
-#Determinar los valores de L2, L3 y L1, al despejar las proporciones en el criterio de rectitud 
-
+#Determinar los valores de L2, L3 y L1, al despejar las proporciones en el criterio de rectitud con 30 grados
+print('\nValores de L1, L2 yL3 en criterio de rectitud con 30 grados: ')
 L2 = delthaX/Inter_grad_ratiosR1(30)
-print('L2: ',L2)
-
+print('\nL2: ',L2)
 L3 = L2*Inter_grad_ratiosR2(30)
 print('L3: ',L3)
-
 L1 = L2*Inter_grad_ratiosR3(30)
 print('L1: ',L1)
+
+#Determinar los valores de L2, L3 y L1, al despejar las proporciones en el criterio de rectitud con 55 grados
+print('\nValores de L1, L2 yL3 en criterio de rectitud con 55 grados: ')
+L_2 = delthaX/Inter_grad_ratiosR1(55)
+print('\nL_2: ',L_2)
+L_3 = L_2*Inter_grad_ratiosR2(55)
+print('L_3: ',L_3)
+L_1 = L_2*Inter_grad_ratiosR3(55)
+print('L_1: ',L_1)
+
+#Determinar los valores de L2, L3 y L1, al despejar las proporciones en el criterio de velocidad con 30 grados
+print('\nValores de L1, L2 yL3 en criterio de velocidad con 30 grados: ')
+l2 = delthaX/Inter_grad_ratiosV1(30)
+print('\nL2: ',l2)
+l3 = l2*Inter_grad_ratiosV2(30)
+print('L3: ',l3)
+l1 = l2*Inter_grad_ratiosV3(30)
+print('L1: ',l1)
+
+#Determinar los valores de L2, L3 y L1, al despejar las proporciones en el criterio de velocidad con 55 grados
+print('\nValores de L1, L2 yL3 en criterio de velocidad con 55 grados: ')
+l_2 = delthaX/Inter_grad_ratiosV1(55)
+print('\nL2: ',l_2)
+l_3 = l_2*Inter_grad_ratiosV2(55)
+print('L3: ',l_3)
+l_1 = l_2*Inter_grad_ratiosV3(55)
+print('L1: ',l_1)
 
 #Evaluar los polinomios en un intervalo 
 t = np.linspace(20,180,200)
@@ -53,27 +71,65 @@ y1 = Inter_grad_ratiosR1(t)
 y2 = Inter_grad_ratiosR2(t)
 y3 = Inter_grad_ratiosR3(t)
 
-#Grafica de las interpolaciones
+x1 = Inter_grad_ratiosV1(t)
+x2 = Inter_grad_ratiosV2(t)
+x3 = Inter_grad_ratiosV3(t)
 
-plt.figure('1')
-plt.plot(deltha_grad, ratiosR1, mew=2, label='Datos')
-plt.plot(t, y1, label='Interpolacion')
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.legend()
+#Gráfica de las interpolaciones
 
-plt.figure('2')
-plt.plot(deltha_grad, ratiosR2, mew=2, label='Datos')
-plt.plot(t, y2, label='Interpolación 2')
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.legend()
-plt.show()
+#Interpolaciones empleando Splines
+print('\nInterpolaciones empleando Splines: ')
+Inter_ratiosR1 = InterpolatedUnivariateSpline(deltha_grad, ratiosR1)
+Inter_ratiosR2 = InterpolatedUnivariateSpline(deltha_grad, ratiosR2)
+Inter_ratiosR3 = InterpolatedUnivariateSpline(deltha_grad, ratiosR3)
 
-plt.figure('3')
-plt.plot(deltha_grad, ratiosR2, mew=2, label='Datos')
-plt.plot(t, y2, label='Interpolación 3')
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.legend()
-plt.show()
+Inter_ratiosV1 = InterpolatedUnivariateSpline(deltha_grad, ratiosV1)
+Inter_ratiosV2 = InterpolatedUnivariateSpline(deltha_grad, ratiosV2)
+Inter_ratiosV3 = InterpolatedUnivariateSpline(deltha_grad, ratiosV3)
+
+#Determinar los valores de L2, L3 y L1, al despejar las proporciones en el criterio de rectitud con 30 grados
+print('\nValores de L1, L2 yL3 en criterio de rectitud con 30 grados: ')
+l_1_2 = delthaX/Inter_ratiosR1(30)
+print('\nL2: ',l_1_2)
+l_1_3 = l_1_2*Inter_ratiosR2(30)
+print('L3: ',l_1_3)
+l_1_1 = l_1_2*Inter_ratiosR3(30)
+print('L1: ',l_1_1)
+
+#Determinar los valores de L2, L3 y L1, al despejar las proporciones en el criterio de rectitud con 55 grados
+print('\nValores de L1, L2 yL3 en criterio de rectitud con 55 grados: ')
+l_2_2 = delthaX/Inter_ratiosR1(55)
+print('\nL2: ',l_2_2)
+l_2_3 = l_2_2*Inter_ratiosR2(55)
+print('L3: ',l_2_3)
+l_2_1 = l_2_2*Inter_ratiosR3(55)
+print('L1: ',l_2_1)
+
+#Determinar los valores de L2, L3 y L1, al despejar las proporciones en el criterio de velocidad con 30 grados
+print('\nValores de L1, L2 yL3 en criterio de velocidad con 30 grados: ')
+L_1_2 = delthaX/Inter_ratiosV1(30)
+print('\nL2: ',L_1_2)
+L_1_3 = L_1_2*Inter_ratiosV2(30)
+print('L3: ',L_1_3)
+L_1_1 = L_1_2*Inter_ratiosV3(30)
+print('L1: ',L_1_1)
+
+#Determinar los valores de L2, L3 y L1, al despejar las proporciones en el criterio de velocidad con 55 grados
+print('\nValores de L1, L2 yL3 en criterio de velocidad con 55 grados: ')
+L_2_2 = delthaX/Inter_ratiosV1(55)
+print('\nL2: ',L_2_2)
+L_2_3 = L_2_2*Inter_ratiosV2(55)
+print('L3: ',L_2_3)
+L_2_1 = L_2_2*Inter_ratiosV3(55)
+print('L1: ',L_2_1)
+
+#Evaluar los polinomios en un intervalo 
+y_1 = Inter_ratiosR1(t)
+y_2 = Inter_ratiosR2(t)
+y_3 = Inter_ratiosR3(t)
+
+x_1 = Inter_ratiosV1(t)
+x_2 = Inter_ratiosV2(t)
+x_3 = Inter_ratiosV3(t)
+
+#Gráfica de las interpolaciones
